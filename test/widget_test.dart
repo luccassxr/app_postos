0cc7e2d60f7c7846c700cf1a08c36cf5fd6036db
luccassxr <lucas123topop@gmail.com>
2026-08-19 19:58:56 -0300

    test: update app name expectation to Grupo WK

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,10 @@ import 'package:app_postos/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('exibe a tela de acesso do WK Cliente', (tester) async {
+  testWidgets('exibe a tela de acesso do Grupo WK', (tester) async {
     await tester.pumpWidget(const WKClienteApp());
 
-    expect(find.text('WK Cliente'), findsOneWidget);
+    expect(find.text('Grupo WK'), findsOneWidget);
     expect(find.text('Entrar'), findsOneWidget);
     expect(find.text('Criar cadastro'), findsOneWidget);
   });
